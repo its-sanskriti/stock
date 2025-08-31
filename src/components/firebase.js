6ea3a4869abefa7475c74bcc8a237e696a5c5781
Sanskriti Singh <155823572+its-sanskriti@users.noreply.github.com>
2025-08-31 13:57:18 +0530

    actual update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,21 +1,35 @@
 // src/components/firebase.js
+
+
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAQ-8VuwCkrUEI1bkAdwOLqXOfnpeeMDF8",
+  authDomain: "stock-analyzer-94ad0.firebaseapp.com",
+  projectId: "stock-analyzer-94ad0",
+  storageBucket: "stock-analyzer-94ad0.firebasestorage.app",
+  messagingSenderId: "249474623096",
+  appId: "1:249474623096:web:4980844de2638631b52f8d",
+  measurementId: "G-H5TXSSB6S1"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 
 const auth = getAuth(app);
 const database = getDatabase(app);
